f7d8f605ae0a747cf8a79320d03fc1c64957c741
mg_me <mertguleryazilimtest@gmail.com>
2025-07-05 14:34:31 +0300

    test:mertv1

---
+++ src/test/java/Tests/BaseTest.java
@@ -0,0 +1,31 @@
+package Tests;
+
+import Utils.DriverFactory;
+import io.qameta.allure.*;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+@Epic("DriverFactory Turn On And Turn Off")
+@Feature("The Driver Be Active ")
+public class BaseTest {
+
+    private static WebDriver driver;
+
+
+    @BeforeClass(description = "DriverFactory Call It")
+    @Severity(SeverityLevel.CRITICAL)
+    @Story("Wait For The Driver To Be Active")
+    public void setup(){
+        Allure.step("DriverFactory Call It And The Driver Fill");
+        driver = DriverFactory.getDriver();
+    }
+
+    @AfterClass(description = "DriverFactory Turn Off")
+    @Severity(SeverityLevel.CRITICAL)
+    @Story("Wait For The Driver To Be İnactive")
+    public void teardown(){
+      Allure.step("DriverFactory Call It And The Driver Be Emptys");
+      DriverFactory.quitDriver();
+    }
+}
